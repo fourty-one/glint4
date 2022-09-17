fd73064e0f334bd3c9c7d0b450e833807a5c50d2
fourty one <4142140@gmail.com>
2022-09-17 18:15:51 +0600

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,4 @@
+import 'normalize-scss'
+import 'material-icons/iconfont/material-icons.scss';
+import '../scss/style.scss';
+
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        main: path.resolve(__dirname, 'src/js/main.js'),
+    },
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: path.resolve(__dirname, 'src/index.html')
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].[contenthash].css"
+        }),
+        new CleanWebpackPlugin(),
+    ],
+    module: {
+        rules: [
+          {
+            test: /\.s[ac]ss$/i,
+            use: [
+              MiniCssExtractPlugin.loader,
+              'css-loader',
+              'sass-loader',
+            ],
+          },
+          {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
+            generator: {
+              filename: 'static/images/[name].[contenthash].[ext]'
+            }
+          },
+          {
+            test: /\.(woff|woff2|eot|ttf|otf)$/i,
+            type: 'asset/resource',
+            generator: {
+              filename: 'static/fonts/[name].[contenthash].[ext]'
+            }
+          },
+          {
+            test: /\.html$/i,
+            loader: "html-loader",
+          },
+        ],
+    },
+}
