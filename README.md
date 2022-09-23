12e0de391821f143bf3a32780d513a1a7eed9de9
fourty one <4142140@gmail.com>
2022-09-23 16:33:25 +0600

    section works

---
+++ README.md
@@ -0,0 +1,9 @@
+# glint4
+
+## Development server
+
+Run `npm run serve` for a dev server. Navigate to `http://localhost:2000/`. The application will automatically reload if you change any of the source files.
+
+## Build
+
+Run `npm run build` to build the project. The build artifacts will be stored in the `dist/` directory.
--- webpack.config.js
+++ webpack.config.js
@@ -36,14 +36,14 @@ module.exports = {
             test: /\.(png|svg|jpg|jpeg|gif)$/i,
             type: 'asset/resource',
             generator: {
-              filename: 'static/images/[name].[contenthash].[ext]'
+              filename: 'static/images/[name].[contenthash][ext]'
             }
           },
           {
             test: /\.(woff|woff2|eot|ttf|otf)$/i,
             type: 'asset/resource',
             generator: {
-              filename: 'static/fonts/[name].[contenthash].[ext]'
+              filename: 'static/fonts/[name].[contenthash][ext]'
             }
           },
           {
