c223965c723d9802a219db0aefe204499522ca9e
fourty one <4142140@gmail.com>
2022-09-25 16:39:59 +0600

    fix nav links

--- src/js/_vars.js
+++ src/js/_vars.js
@@ -6,6 +6,9 @@ export default {
 	$nav: document.querySelector('.header__nav'),
     $overlay: document.querySelector('.overlay'),
 	$containers: document.querySelectorAll('.container'),
+	$menuOpenButton: document.querySelector('.open-menu-button'),
+	$menuCloseButton: document.querySelector('.close-menu-button'),
+	$navLinks: document.querySelectorAll('.nav__link'),
 	$clientsSlider: document.querySelector('.clients-slider'),
 	$testimonalsSlider: document.querySelector('.testimonals-slider')
 }
--- src/js/components/menu.js
+++ src/js/components/menu.js
@@ -1,11 +1,16 @@
 import vars from '../_vars';
 
-window.openMenu = function() {
-  vars.$overlay.classList.add('overlay-up')
-  vars.$nav.classList.add('nav-open')
+function openMenu() {
+  vars.$overlay.classList.add('overlay-up');
+  vars.$nav.classList.add('nav-open');
 }
 
-window.closeMenu = function() {
-  vars.$nav.classList.remove('nav-open')
-  vars.$overlay.classList.remove('overlay-up')
+function closeMenu() {
+  vars.$nav.classList.remove('nav-open');
+  vars.$overlay.classList.remove('overlay-up');
 }
+
+vars.$menuOpenButton.onclick = openMenu;
+
+[vars.$overlay, vars.$menuCloseButton, ...vars.$navLinks]
+  .map(el => el.onclick = closeMenu);
